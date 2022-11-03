59b1309e3eeb9e52724853974dcbfb14d5c6a1bd
Vaso <vasothom@hotmail.com>
2022-11-03 15:49:42 +0100

    todo

---
+++ to-do/src/components/Todo.js
@@ -0,0 +1,57 @@
+import { Component } from "react";
+import classNames from "classnames";
+import "./Todo.css";
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [], newid: 0 };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTodoClick = this.handleTodoClick.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: this.state.newid,
+      label: e.target[0].value,
+      done: false,
+    };
+    if (!newTodo.label) return;
+    this.setState((state) => ({
+      todos: [...state.todos, newTodo],
+      newid: state.newId + 1,
+    }));
+    e.target[0].value = "";
+  }
+  handleTodoClick(index) {
+    const updatedtodos = JSON.parse(JSON.stringify(this.state.todos));
+    updatedtodos[index].done = !updatedtodos[index].done;
+
+    this.setState((state) => ({ ...state, todos: updatedtodos }));
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>To-do : My List</h1>
+          {this.state.todos.map((todo, index) => (
+            <div
+              id="input"
+              key={todo.id}
+              onClick={() => this.handleTodoClick(index)}
+              className={classNames({ done: todo.done })}
+            >
+              {todo.label}
+            </div>
+          ))}
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="todo" />
+            <input className="button" type="submit" value="add todo" />
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+export default Todo;
